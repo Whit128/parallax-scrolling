2b8a7f87f5073ceebc03e68b993c374dad060e23
bogdanfer <ferariu_boby@yahoo.com>
2017-08-10 01:29:31 +0200

    IE11 bug fix, JS scope and markup option

--- scroll.js
+++ scroll.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const moving__background = document.querySelector('#header__background');
+    const parallax__img = moving__background.getAttribute('data-image');
+    const parallaxOffset = moving__background.offsetHeight;
 
+    // Set parallax image as background.
+    if ( parallax__img ) {
+    	moving__background.style.backgroundImage = "url('" + parallax__img +"')";
+    }
+ 
+ 	// Bind scroll event
     window.addEventListener( 'scroll', function() {
-        ParallaxAnimation( moving__background );
-    });
+        this.ParallaxAnimation( moving__background );
+    }.bind(this));
+
+	this.ParallaxAnimation = function( el ) {
+	    let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+
+	    if (scrollTop <= parallaxOffset) {
+		    el.style.marginTop = (scrollTop / 3) + 'px';
+		    el.style.opacity = (1 - scrollTop / el.offsetHeight );
+	    }
+	};
 });
-
-function ParallaxAnimation( el ) {
-    let scrollTop = document.body.scrollTop;
-
-    el.style.marginTop = (scrollTop / 3) + 'px';
-    el.style.opacity = (1 - scrollTop / el.offsetHeight );
-};
